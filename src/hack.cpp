39933b5004d1cf67e2c5f91ad900507de5cac3e2
Eliott Johnson <eliott.johnson@gmail.com>
2020-02-26 15:18:57 +0100

    changed output format

--- src/hack.cpp
+++ src/hack.cpp
@@ -15,13 +15,14 @@ int main()
 {
 
   std::cout<<std::endl;
-  std::cout<<"███████╗██╗     ██╗ ██████╗ ████████╗████████╗██╗     ███████╗██████╗  ██████╗ ██╗   ██╗ ██████╗ ███████╗"<<std::endl;
-  std::cout<<"██╔════╝██║     ██║██╔═══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝██╔══██╗██╔═══██╗██║   ██║██╔════╝ ██╔════╝"<<std::endl;
-  std::cout<<"█████╗  ██║     ██║██║   ██║   ██║      ██║   ██║     █████╗  ██████╔╝██║   ██║██║   ██║██║  ███╗█████╗  "<<std::endl;
-  std::cout<<"██╔══╝  ██║     ██║██║   ██║   ██║      ██║   ██║     ██╔══╝  ██╔══██╗██║   ██║██║   ██║██║   ██║██╔══╝  "<<std::endl;
-  std::cout<<"███████╗███████╗██║╚██████╔╝   ██║      ██║   ███████╗███████╗██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████╗"<<std::endl;
-  std::cout<<"╚══════╝╚══════╝╚═╝ ╚═════╝    ╚═╝      ╚═╝   ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝"<<std::endl;
-  std::cout<<"                                                                                                         "<<std::endl;
+
+std::cout<<" █████╗ ██╗     ██████╗ ██╗ ██████╗ ███╗   ██╗██╗  ██╗ █████╗  ██████╗██╗  ██╗"<<std::endl;
+std::cout<<"██╔══██╗██║     ██╔══██╗██║██╔═══██╗████╗  ██║██║  ██║██╔══██╗██╔════╝██║ ██╔╝"<<std::endl;
+std::cout<<"███████║██║     ██████╔╝██║██║   ██║██╔██╗ ██║███████║███████║██║     █████╔╝ "<<std::endl;
+std::cout<<"██╔══██║██║     ██╔══██╗██║██║   ██║██║╚██╗██║██╔══██║██╔══██║██║     ██╔═██╗ "<<std::endl;
+std::cout<<"██║  ██║███████╗██████╔╝██║╚██████╔╝██║ ╚████║██║  ██║██║  ██║╚██████╗██║  ██╗"<<std::endl;
+std::cout<<"╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝"<<std::endl;
+std::cout<<"                                                              by Eliottlerouge\n"<<std::endl;
 
   dataLoader *dLoader = new dataLoader();
   
@@ -58,29 +59,40 @@ int main()
       const std::string sell_price_min_date(jsonDataCaerleon[i]["sell_price_min_date"].asString());
       
 
-       std::string data = buy_price_min_date;
-       dt.clear();
-       if (strtk::string_to_type_converter(data, fmt))
-       {/*
-          std::cout << "Date-Time: " << data      ;
-          std::cout << "Year:   "    << dt.year   ;
-          std::cout << "Month:  "    << dt.month  ;
-          std::cout << "Day:    "    << dt.day    ;
-          std::cout << "Hour:   "    << dt.hour   ;
-          std::cout << "Minute: "    << dt.minute ;
-          std::cout << "Second: "    << dt.second ;
-          */
-       }
-       else{
-        std::cout << "Error occured during parsing of date-time: " << data << "\n";
-       }
+     std::string data = buy_price_min_date;
+     std::string data2 = sell_price_min_date;
+     
+     /*
+     dt.clear();
+     if (strtk::string_to_type_converter(data, fmt))
+     {
+        std::cout << "Date-Time: " << data      ;
+        std::cout << "Year:   "    << dt.year   ;
+        std::cout << "Month:  "    << dt.month  ;
+        std::cout << "Day:    "    << dt.day    ;
+        std::cout << "Hour:   "    << dt.hour   ;
+        std::cout << "Minute: "    << dt.minute ;
+        std::cout << "Second: "    << dt.second ;
+        
+     }
+     else{
+      std::cout << "Error occured during parsing of date-time: " << data << "\n";
+     }
+     */
        
       int benefit = (buy_price_min-(3*buy_price_min/100))-sell_price_min;
       if (benefit>1000 && sell_price_min!=0){ //implement a check time must be less then 1 hour
-        std::cout<<jsonDataCaerleon[i]["item_id"]<<" quality: "<<jsonDataCaerleon[i]["quality"]<<" "<<jsonDataCaerleon[i]["city"]<<"sell price min: "<<jsonDataCaerleon[i]["sell_price_min"]
+        dt.clear();
+        strtk::string_to_type_converter(data, fmt);
+        std::cout<<jsonDataCaerleon[i]["city"]<<"      sell price min: "<<jsonDataCaerleon[i]["sell_price_min"]
+                                                            <<" "<<dt.day<<"-"<<dt.month<<" "<<dt.hour<<"h"<<dt.minute
+                                                            <<"   "<<jsonDataCaerleon[i]["item_id"]<<" quality: "<<jsonDataCaerleon[i]["quality"]<<std::endl;
+
+        dt.clear();
+        strtk::string_to_type_converter(data2, fmt);
+        std::cout<<jsonDataBlackMarket[i]["city"]<<"   buy price min: "<<jsonDataBlackMarket[i]["buy_price_min"]
                                                             <<" "<<dt.day<<"-"<<dt.month<<" "<<dt.hour<<"h"<<dt.minute<<std::endl;
-        std::cout<<jsonDataBlackMarket[i]["item_id"]<<" quality: "<<jsonDataBlackMarket[i]["quality"]<<" "<<jsonDataBlackMarket[i]["city"]<<" buy price min: "<<jsonDataBlackMarket[i]["buy_price_min"]<<std::endl;
-        std::cout<<"  Benefit: "<<benefit<<std::endl;
+        std::cout<<std::string(23,' ')+"Benefit: "<<benefit<<std::endl;
         //std::cout << jsonDataCaerleon.toStyledString() << std::endl;
         //std::cout << jsonDataBlackMarket.toStyledString() << std::endl;
       }
